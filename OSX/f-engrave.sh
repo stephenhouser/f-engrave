9cd415e2495e9bb694ab5d5deded2ca615652e62
Stephen Houser <stephenhouser@gmail.com>
2016-01-06 12:35:15 -0500

    fix startup to include system fonts on f-engrave command line

--- OSX/f-engrave.sh
+++ OSX/f-engrave.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 cd $DIR
-/usr/bin/env python f-engrave-154.py
+PATH=".:$PATH" /usr/bin/env python f-engrave-154.py --fontdir /Library/Fonts --defdir ~/Documents
