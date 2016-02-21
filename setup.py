240cd3d9e5ae620d16ac6333309cede54ce69288
Stephen Houser <stephenhouser@gmail.com>
2016-02-21 11:51:04 -0500

    update application setup to 1.57

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ import sys
 from setuptools import setup
 
 mainscript = 'f-engrave.py'
-version = "1.56"
+version = "1.57"
 url = "https://github.com/stephenhouser/f-engrave"
 
 if sys.platform == 'darwin':
